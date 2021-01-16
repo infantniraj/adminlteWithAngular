28ad823c21088d9ed0b47f90e2d784168bcec165
infant_niroj <infant.niroj@artofliving.org>
2021-01-17 05:16:26 +0530

    latest changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UserComponent } from './pages/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "",
+    component : DashboardComponent
+  },
+  {
+    path : "user",
+    component : UserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { RightSideBreadcomsComponent } from './right-side-breadcoms/right-side-breadcoms.component';
+import { UserComponent } from './pages/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    DashboardComponent,
+    FooterComponent,
+    UserComponent,
+    RightSideBreadcomsComponent
   ],
   imports: [
     BrowserModule,
